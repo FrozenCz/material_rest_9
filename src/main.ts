decc946c0c8f2e64f8e53026f8a84c28ddc45058
FrozenCz <info@milanknop.cz>
2023-03-16 22:10:21 +0100

    tesyt s certifikatem

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,43 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { json, urlencoded } from "express";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { json, urlencoded } from 'express';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  let sslConfig = {};
+
+  if (process.env.ssl) {
+    sslConfig = {
+      httpOptions: {
+        key: fs.readFileSync(
+          '/etc/letsencrypt/live/dp-rest.milanknop.cz/privkey.pem',
+        ),
+        cert: fs.readFileSync(
+          '/etc/letsencrypt/live/dp-rest.milanknop.cz/fullchain.pem',
+        ),
+      },
+    };
+  }
+
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    sslConfig,
+  );
 
   const options = new DocumentBuilder()
-    .setTitle("BP - backend")
-    .setDescription("Milan Knop")
-    .setVersion("0.1")
-    .addTag("backend")
+    .setTitle('BP - backend')
+    .setDescription('Milan Knop')
+    .setVersion('0.1')
+    .addTag('backend')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
 
-  app.use(json({ limit: "200mb" }));
-  app.use(urlencoded({ limit: "200mb", extended: true }));
+  app.use(json({ limit: '200mb' }));
+  app.use(urlencoded({ limit: '200mb', extended: true }));
   await app.listen(3000);
 }
 
